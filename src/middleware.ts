a5d48b34817e52f3e42147cbd3f98079739facac
dajotor <81014941+dajotor@users.noreply.github.com>
2026-04-17 10:29:22 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,12 @@
-import type { NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
 import { updateSession } from "@/lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/api/cron/")) {
+    return NextResponse.next();
+  }
+
   return updateSession(request);
 }
 
